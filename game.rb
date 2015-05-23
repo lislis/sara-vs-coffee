081d2a2eee43a5f3ed1da0eb2f9d6564ef7b1600
Lisa Passing <mail@lislis.de>
2015-05-23 15:50:56 +0200

    add counter and computer classes

--- game.rb
+++ game.rb
@@ -7,8 +7,15 @@ class GameWindow < Gosu::Window
 
     @player = Player.new
     @player.warp(300, 300)
+
+    @barista = Barista.new
+    @barista.warp(720, 40)
+
+    @counter = Counter.new
+    @computer = Computer.new
+
     @font = Gosu::Font.new(20)
-   end
+  end
 
   def update
 
@@ -22,10 +29,15 @@ class GameWindow < Gosu::Window
       @player.accelerate
     end
     @player.move
+
+    @barista.moving
   end
 
   def draw
+    @counter.draw
     @player.draw
+    @barista.draw
+    @computer.draw
     @font.draw("Coffees consumed: #{@player.cups}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_fff00)
   end
 
@@ -92,7 +104,77 @@ class Player
 end
 
 module ZOrder
-  Background, Stars, Player, UI = *0..3
+  Background, Barista, Player, UI = *0..3
+end
+
+class Barista
+
+  attr_accessor :is_preping, :bribe_level
+
+  def initialize
+    @image = Gosu::Image.new('assets/player.png')
+    @prep_time = 4
+    @is_preping = false
+    @is_talking = false
+    @bribe_level = 0
+
+    @y_min = 20
+    @y_max = 550
+    @x = @y = 0
+  end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+
+  def moving
+    if !@is_talking then
+      
+    end
+  end
+
+  def draw
+    @image.draw(@x, @y, ZOrder::Barista)
+  end
+end
+
+class Counter
+
+  def initialize
+    @image = Gosu::Image.new('assets/counter.png')
+  end
+
+  def draw
+    @image.draw(680, 0, ZOrder::Background)
+  end
+end
+
+class Computer
+
+  def initialize
+    @image = Gosu::Image.new('assets/player.png')
+    @is_visible = true
+    @x = @y = 0
+  end
+
+  def randNum
+    0..600.to_a.sample
+  end
+
+  def randPos
+    @x, @y = randNum, randNum
+  end
+
+  def toggleVisibility
+    @is_visible = !!@is_visible
+  end
+
+  def draw
+    if @is_visible then
+      @image.draw(@x, @y, ZOrder::Barista)
+    end
+  end
+
 end
 
 window = GameWindow.new
